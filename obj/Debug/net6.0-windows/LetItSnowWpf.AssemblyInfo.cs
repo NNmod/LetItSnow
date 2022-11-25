7a0c55e3bdfa5d7ec996859b44cadbad923a330b
NNmod <nickvarvarik@gmail.com>
2022-11-25 12:09:57 +0200

    recommit

--- obj/Debug/net6.0-windows/LetItSnowWpf.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/LetItSnowWpf.AssemblyInfo.cs
@@ -11,8 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LetItSnowWpf")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NNmod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Add snowfall background control")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LetItSnowWpf")]
